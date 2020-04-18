1b87bca58c86735ea2cf65d21484b200b8480619
vikasrinvi <vikasmrnv@gmail.com>
2020-04-18 22:05:24 +0530

    imag fluid and code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,9 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="keywords" content="{{ config('blog.meta.keywords') }}">
     <meta name="description" content="{{ config('blog.meta.description') }}">
-
+    
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="google-site-verification" content="HeQPL9wxgcyQM-5zjq5dgXPXAt8FzJ7ddqQ-mUJu-gU" />
 
     <link rel="shortcut icon" href="{{ config('blog.default_icon') }}">
 
