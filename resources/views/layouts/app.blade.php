99081243d029604e4a62661a937c6da662e79fda
vikasrinvi <vikasmrnv@gmail.com>
2020-05-19 01:46:40 +0530

    google analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,15 @@
     </script>
 
     @yield('styles')
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-167000093-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-167000093-1');
+    </script>
 
 
 
