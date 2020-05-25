416a780c32b2dd756dadf4391ce36fa56dfd274f
vikasrinvi <vikasmrnv@gmail.com>
2020-05-26 01:09:42 +0530

    amazon

--- resources/views/particals/jumbotron.blade.php
+++ resources/views/particals/jumbotron.blade.php
@@ -2,6 +2,12 @@
     <div class="container">
         <div class="row text-center">
             <div class="col-md-10 offset-md-1">
+            	 <script type="text/javascript" language="javascript">
+				      var aax_size='728x90';
+				      var aax_pubname = 'vikas0af6-21';
+				      var aax_src='302';
+				    </script>
+				  <script type="text/javascript" language="javascript" src="http://c.amazon-adsystem.com/aax2/assoc.js"></script>
                 {{ $slot }}
             </div>
         </div>
